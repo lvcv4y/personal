b6f4002ab801b78f3c897fa70cdcae9b8dabcb1c
Lvc V4y <98264870+lvcv4y@users.noreply.github.com>
2022-04-09 13:38:18 +0200

    Added little socket project

---
+++ socket/client.py
@@ -0,0 +1,48 @@
+﻿import socket
+import threading
+
+"""
+Little UDP chatting client. Connect to "server.py" to chat with other clients.
+Note: be careful to correctly set the server's ip and port, and not to set an already-in-use port.
+"""
+
+SERVER = ('localhost', 1234)  # (ip, port)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+user = input('name>')
+port = int(input('port>'))
+s.bind(('localhost', port))
+
+
+def receiving_process():
+    while 1:
+        global user
+        data = s.recvfrom(1024)[0]
+        data = data.decode('utf-8')
+        if data[:3] == 'new':
+            print('{} nous a rejoint'.format(data[4:]))
+        elif data[:3] == 'dis':
+            print('{} est parti'.format(data[4:]))
+        else:
+            data = data.split(':')
+            if data[0] != user:
+                print('<{}> : {}'.format(data[0], data[1]))
+
+
+thr = threading.Thread(target=receiving_process)
+thr.daemon = True
+thr.start()
+
+s.sendto('new {}'.format(user).encode('utf8'), SERVER)
+
+while 1:
+    msg = input('')
+    if msg == 'disconnect':
+        s.sendto(('dis ' + user).encode('utf8'), SERVER)
+        s.close()
+        break
+    msg = "{}:{}".format(user, msg)
+    s.sendto(msg.encode('utf8'), SERVER)
+
+
---
+++ socket/server.py
@@ -0,0 +1,34 @@
+﻿import socket
+import threading
+
+"""
+Little UDP chatting server. It allows multiple connections at once.
+"""
+
+PORT = 1234
+
+client = []
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(("localhost", PORT))
+print("[*] Server is running...")
+
+while 1:
+    try:
+        data, addr = s.recvfrom(1024)
+    except IOError as e:
+        print(e)
+        continue
+
+    print('[*] data received from {} : "{}"'.format(addr, data))
+    if addr not in client:
+        client.append(addr)
+        for c in client:
+            s.sendto(data, c)
+    else:
+        if data.decode('utf-8')[:3] == 'dis':
+            for c in client:
+                s.sendto("dis {}".format(data.decode('utf-8')[4:]).encode('utf-8'), c)
+            client.remove(addr)
+        else:
+            for c in client:
+                s.sendto(data, c)
